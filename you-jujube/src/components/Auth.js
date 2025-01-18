12be1fe5b2c1070840d26a0bb8c181fdc52e76d0
Lucie Yang <luya.495@gmail.com>
2025-01-18 11:24:47 -0500

    Implement Firebase integration for user data management in Home component

--- you-jujube/src/components/Auth.js
+++ you-jujube/src/components/Auth.js
@@ -1,22 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "reactstrap";
 
-import { initializeApp } from "firebase/app";
-import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+// import { initializeApp } from "firebase/app";
+// import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+import { db, getDoc, setDoc, doc } from "../firebase-config"; 
 
 export const LoginButton = () => {
   const { loginWithPopup, getIdTokenClaims, user, isAuthenticated } = useAuth0();
