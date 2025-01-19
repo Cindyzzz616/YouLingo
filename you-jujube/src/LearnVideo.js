1c8f54d2af3f6a5b39bb9ec1952610299fb5bb53
NOBODIDI <adrienmeryusa@gmail.com>
2025-01-18 20:33:00 -0500

    display video data on LearnVideo

--- you-jujube/src/LearnVideo.js
+++ you-jujube/src/LearnVideo.js
@@ -1,29 +1,34 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Row, Col, Card, CardBody, CardTitle, CardText } from 'reactstrap';
-import YouTube from 'react-youtube';
-import { useParams } from 'react-router-dom';
-import { useAuth0 } from '@auth0/auth0-react';
-import { db, getDoc, setDoc, doc } from "./firebase-config"; 
+import React, { useState, useEffect } from "react";
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  CardBody,
+  CardTitle,
+  CardText,
+} from "reactstrap";
+import YouTube from "react-youtube";
+import { useParams, useLocation } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import { db, getDoc, setDoc, doc } from "./firebase-config";
 import { updateDoc, increment } from "firebase/firestore";
-import { fetchVideoCount } from './services/viewCounterService';
+import { fetchVideoCount } from "./services/viewCounterService";
 
 const LearnVideo = () => {
   const { videoId } = useParams();
-  console.log(videoId);
+  const location = useLocation();
+  const { videoInfo } = location.state || {};
+  console.log("Video Info:", videoInfo); // Ensure videoInfo is logged
   const { user, isAuthenticated, isLoading } = useAuth0();
   const [count, setCount] = useState(0);
 
-  const transcript = `
-    This is the transcript of the video. You can replace this with actual content.
-    It will display at the bottom of the video player.
-    The transcript helps users follow along with the video.
-    You can style this content and make it more interactive later.
-  `;
+  const transcript = videoInfo.transcripts;
 
   // YouTube player options
   const opts = {
-    height: '390',
-    width: '640',
+    height: "390",
+    width: "640",
     playerVars: {
       autoplay: 1, // Autoplay the video
     },
@@ -33,23 +38,25 @@ const LearnVideo = () => {
     console.log(`Recording video ${videoId} watch history...`);
 
     try {
-      const userRef = doc(db, "users", user.sub); 
-      const userDoc = await getDoc(userRef); 
+      const userRef = doc(db, "users", user.sub);
+      const userDoc = await getDoc(userRef);
 
       if (userDoc.exists()) {
-        const history = userDoc.data().history || {}; 
-  
+        const history = userDoc.data().history || {};
+
         if (history[videoId]) {
           history[videoId] += 1;
         } else {
           history[videoId] = 1;
         }
-  
+
         await updateDoc(userRef, {
-          history: history, 
+          history: history,
         });
-  
-        console.log(`Video ${videoId} watch history updated. Current count: ${history[videoId]}`);
+
+        console.log(
+          `Video ${videoId} watch history updated. Current count: ${history[videoId]}`
+        );
         setCount(history[videoId]);
       } else {
         console.log("User document does not exist.");
@@ -67,39 +74,38 @@ const LearnVideo = () => {
       };
 
       getVideoCount();
-    };
-
+    }
   }, [videoId, isAuthenticated, user?.sub]);
 
   if (isLoading) {
-    return <div>Loading...</div>; 
+    return <div>Loading...</div>;
   }
 
   return (
-    <Container 
+    <Container
       style={{
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        textAlign: 'center',
-      }}>
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        textAlign: "center",
+      }}
+    >
       <Row>
         <Col xs="12" md="8" className="mx-auto">
           <Card>
-            <CardBody>
-              <YouTube 
-                videoId={videoId} 
-                opts={opts} 
-                onEnd={recordHistory}
-              />
+            <CardBody style={{ textAlign: "left" }}>
+              <YouTube videoId={videoId} opts={opts} onEnd={recordHistory} />
               <CardText>
-                <strong>Language Difficulty Level:</strong> Intermediate
+                <strong>Final Levels:</strong>
+                {JSON.stringify(videoInfo?.final_levels)}
               </CardText>
               {/* TODO: use API for this */}
               <CardText>
                 <strong>Times Watched:</strong> {count}
               </CardText>
-              <CardTitle className="mt-4" style={{ fontWeight: 'bold' }}>Transcript</CardTitle>
+              <CardTitle className="mt-4" style={{ fontWeight: "bold" }}>
+                Transcript
+              </CardTitle>
               <CardText>
                 <pre>{transcript}</pre>
               </CardText>
