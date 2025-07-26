45f1099c94c038957e294831131b17d3db875caf
Cindyzzz616 <cxy.zhang@mail.utoronto.ca>
2025-07-26 12:39:56 -0400

    pickled test objects and implemented syntactic categories of word list

--- video_analysis/nltk_downloads.py
+++ video_analysis/nltk_downloads.py
@@ -2,5 +2,14 @@ import nltk
 nltk.download("cmudict")
 nltk.download('averaged_perceptron_tagger_eng')
 
+nltk.download('punkt_tab')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('tagsets_json')
+
+nltk.help.upenn_tagset()
+tokens = nltk.word_tokenize("Hello world, this is a test.")
+tags = nltk.pos_tag(tokens)
+print(tags)
+
 # use this ./.venv/bin/python -m pip install package to specify where the package should be installed
 # activate the virtual environment: source .venv/bin/activate
--- video_analysis/test_objects.py
+++ video_analysis/test_objects.py
@@ -1,7 +1,30 @@
+import pickle
+
 from Video import Video
 from User import User
 
-video_etymology = Video(path="video_analysis/videos/etymology.MP4")
-video_linguistic_intelligence = Video(path="video_analysis/videos/linguistic_intelligence.MP4")
+# Initialize the objects and save them to disk - only run after changes are made
+# video_etymology = Video(path="video_analysis/videos/etymology.MP4")
+# video_linguistic_intelligence = Video(path="video_analysis/videos/linguistic_intelligence.MP4")
 
-user = User(vocab_size=3000, l1='Mandarin Chinese')
+# user = User(vocab_size=3000, l1='Mandarin Chinese')
+
+
+# with open("video_object_etymology.pkl", "wb") as f:
+#     pickle.dump(video_etymology, f)
+
+# with open("video_object_linguistic_intelligence.pkl", "wb") as f:
+#     pickle.dump(video_linguistic_intelligence, f)
+
+# with open("user_object.pkl", "wb") as f:
+#     pickle.dump(user, f)
+
+# Load the objects from disk
+with open("video_object_etymology.pkl", "rb") as f:
+    video_etymology = pickle.load(f)
+
+with open("video_object_linguistic_intelligence.pkl", "rb") as f:
+    video_linguistic_intelligence = pickle.load(f)
+
+with open("user_object.pkl", "rb") as f:
+    user = pickle.load(f)
